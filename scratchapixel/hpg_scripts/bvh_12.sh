d41df15df5f77856b53538a4ffbc8aa222094020
Khaled Hassan <khaled.hassan@gmail.com>
2017-04-23 14:05:18 -0400

    fix bvh hpg scripts

--- scratchapixel/hpg_scripts/bvh_12.sh
+++ scratchapixel/hpg_scripts/bvh_12.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=trianglemesh      # Job name
+#SBATCH --job-name=bvh      # Job name
 #SBATCH --mail-type=ALL              # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=khaledjhassan@ufl.edu  # Where to send mail	
 #SBATCH --nodes=1                    # Use one node
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=12            # Number of CPU cores per task
 #SBATCH --mem=1gb                  # Total memory limit
 #SBATCH --time=00:30:00              # Time limit hrs:min:sec
-#SBATCH --output=trianglemesh_12_%j.out     # Standard output and error log
+#SBATCH --output=bvh_12_%j.out     # Standard output and error log
 #SBATCH --dependency=singleton       # only run 1 at a time because the program creates files and I don't want them to fight
 
 pwd; hostname; date
@@ -18,6 +18,6 @@ module load gcc/5.2.0
 
 cd /home/khaledjhassan/ray-tracing/scratchapixel/bvh
 
-time ./trianglemesh
+time ./acceleration
 
 date
